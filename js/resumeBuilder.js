9ddedf278c8c4b0e980ac173fb5f98ed1441f522
Chris C <casson@purduecal.edu>
2016-12-29 16:14:36 -0600

    Add bio JavaScript object and me.jpg.

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,8 +1,34 @@
+var bio = {
+    "name" : "Chris Casson",
+    "role" : "Java Developer",
+    "contacts" : {
+        "mobile" : "219-730-3993",
+        "email" : "cassonchris723@gmail.com",
+        "github" : "cassonchris",
+        "location" : "Chicago, IL"
+    },
+    "bioPic" : "images/me.jpg",
+    "skills" : [ "Java", "SQL" ]
+}
+
 var name = "Chris Casson";
 var role = "Java Developer";
 
-var formattedName = HTMLheaderName.replace("%data%", name);
-var formattedRole = HTMLheaderRole.replace("%data%", role);
+var formattedName = HTMLheaderName.replace("%data%", bio.name);
+var formattedRole = HTMLheaderRole.replace("%data%", bio.role);
+var formattedMobile = HTMLmobile.replace("%data%", bio.contacts.mobile);
+var formattedEmail = HTMLemail.replace("%data%", bio.contacts.email);
+var formattedGithub = HTMLgithub.replace("%data%", bio.contacts.github);
+var formattedLocation = HTMLlocation.replace("%data%", bio.contacts.location);
+var formattedBioPic = HTMLbioPic.replace("%data%", bio.bioPic);
+var formattedSkills = HTMLskills.replace("%data%", bio.skills);
 
+$("#header").prepend(HTMLskillsStart);
+$("#skills").prepend(formattedSkills);
+$("#header").prepend(formattedBioPic);
+$("#header").prepend(formattedLocation);
+$("#header").prepend(formattedGithub);
+$("#header").prepend(formattedEmail);
+$("#header").prepend(formattedMobile);
 $("#header").prepend(formattedRole);
 $("#header").prepend(formattedName);
