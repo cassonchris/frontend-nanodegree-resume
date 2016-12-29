03dab970cd6863505ae2a797342048e43dad281c
Chris C <casson@purduecal.edu>
2016-12-29 16:37:32 -0600

    Clean resumeBuilder.js so that only the objects remain.

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -40,25 +40,3 @@ var education = {
         }
     ]
 };
-
-var name = "Chris Casson";
-var role = "Java Developer";
-
-var formattedName = HTMLheaderName.replace("%data%", bio.name);
-var formattedRole = HTMLheaderRole.replace("%data%", bio.role);
-var formattedMobile = HTMLmobile.replace("%data%", bio.contacts.mobile);
-var formattedEmail = HTMLemail.replace("%data%", bio.contacts.email);
-var formattedGithub = HTMLgithub.replace("%data%", bio.contacts.github);
-var formattedLocation = HTMLlocation.replace("%data%", bio.contacts.location);
-var formattedBioPic = HTMLbioPic.replace("%data%", bio.bioPic);
-var formattedSkills = HTMLskills.replace("%data%", bio.skills);
-
-$("#header").prepend(HTMLskillsStart);
-$("#skills").prepend(formattedSkills);
-$("#header").prepend(formattedBioPic);
-$("#header").prepend(formattedLocation);
-$("#header").prepend(formattedGithub);
-$("#header").prepend(formattedEmail);
-$("#header").prepend(formattedMobile);
-$("#header").prepend(formattedRole);
-$("#header").prepend(formattedName);
