836abcd432dfb5802a93845947b8a1c9f9dd31b9
Chris C <casson@purduecal.edu>
2016-12-29 15:15:40 -0600

    Add fun thoughts to the page.

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -3,3 +3,7 @@ $("#main").append("Chris Casson");
 var awesomeThoughts = "I am Chris and I am AWESOME!";
 
 console.log(awesomeThoughts);
+
+var funThoughts = awesomeThoughts.replace("AWESOME", "FUN");
+
+$("#main").append(funThoughts);
