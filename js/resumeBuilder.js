5f48b1358efd08bc6e42e09398b822df81cf43f0
Chris C <casson@purduecal.edu>
2017-01-02 01:06:50 -0600

    Make the objects match the schema exactly.

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -7,6 +7,7 @@ var bio = {
         "github" : "cassonchris",
         "location" : "Chicago, IL"
     },
+    "welcomeMessage" : "Hello!",
     "bioPic" : "images/me.jpg",
     "skills" : [ "Java", "SQL" ],
     "display" : function() {
@@ -17,11 +18,13 @@ var bio = {
         var formattedGithub = HTMLgithub.replace("%data%", this.contacts.github);
         var formattedLocation = HTMLlocation.replace("%data%", this.contacts.location);
         var formattedBioPic = HTMLbioPic.replace("%data%", this.bioPic);
+        var formattedWelcomMessage = HTMLwelcomeMsg.replace("%data%", this.welcomeMessage);
 
         $("#header").prepend(formattedRole);
         $("#header").prepend(formattedName);
                 
         $("#header").append(formattedBioPic);
+        $("#header").append(formattedWelcomMessage);
         
         if (this.skills.length > 0) {
             $("#header").append(HTMLskillsStart);
@@ -81,68 +84,80 @@ var education = {
         {
             "name" : "Purdue Calumet",
             "degree" : "Master of Science",
-            "major" : "Computer Science",
+            "majors" : [ "Computer Science" ],
             "location" : "Hammond, IN",
-            "dates" : "2013 - 2017"
+            "dates" : "2013 - 2017",
+            "url" : "pnw.edu"
         },
         {
             "name" : "Purdue North Central",
             "degree" : "Bachelor of Science",
-            "major" : "Computer Information Technology",
+            "majors" : [ "Computer Information Technology", "Electrical Computer Engineering Technology" ],
             "location" : "Westville, IN",
-            "dates" : "2009 - 2013"
+            "dates" : "2009 - 2013",
+            "url" : "pnw.edu"
         }
     ],
     "onlineCourses" : [
         {
             "title" : "Android Development for Beginners",
             "school" : "Udacity",
-            "url" : "https://www.udacity.com/course/android-development-for-beginners--ud837"
+            "url" : "https://www.udacity.com/course/android-development-for-beginners--ud837",
+            "dates" : "2016"
         },
         {
             "title" : "Intro to HTML and CSS",
             "school" : "Udacity",
-            "url" : "https://www.udacity.com/course/intro-to-html-and-css--ud304"
+            "url" : "https://www.udacity.com/course/intro-to-html-and-css--ud304",
+            "dates" : "2016"
         },
         {
             "title" : "Intro to Computer Science",
             "school" : "Udacity",
-            "url" : "https://www.udacity.com/course/intro-to-computer-science--cs101"
+            "url" : "https://www.udacity.com/course/intro-to-computer-science--cs101",
+            "dates" : "2013"
         },
         {
             "title" : "JavaScript Basics",
             "school" : "Udacity",
-            "url" : "https://www.udacity.com/course/javascript-basics--ud804"
+            "url" : "https://www.udacity.com/course/javascript-basics--ud804",
+            "dates" : "2016"
         },
         {
             "title" : "How to Use Git and GitHub",
             "school" : "Udacity",
-            "url" : "https://www.udacity.com/course/how-to-use-git-and-github--ud775"
+            "url" : "https://www.udacity.com/course/how-to-use-git-and-github--ud775",
+            "dates" : "2015"
         },
         {
             "title" : "Programming Foundations with Python",
             "school" : "Udacity",
-            "url" : "https://www.udacity.com/course/programming-foundations-with-python--ud036"
+            "url" : "https://www.udacity.com/course/programming-foundations-with-python--ud036",
+            "dates" : "2016"
         },
         {
             "title" : "Intro to Relational Databases",
             "school" : "Udacity",
-            "url" : "https://www.udacity.com/course/intro-to-relational-databases--ud197"
+            "url" : "https://www.udacity.com/course/intro-to-relational-databases--ud197",
+            "dates" : "2016"
         },
         {
             "title" : "Full Stack Foundations",
             "school" : "Udacity",
-            "url" : "https://www.udacity.com/course/full-stack-foundations--ud088"
+            "url" : "https://www.udacity.com/course/full-stack-foundations--ud088",
+            "dates" : "2016"
         },
         {
             "title" : "Data Wrangling with MongoDB",
             "school" : "Udacity",
-            "url" : "https://www.udacity.com/course/data-wrangling-with-mongodb--ud032"
+            "url" : "https://www.udacity.com/course/data-wrangling-with-mongodb--ud032",
+            "dates" : "2016"
         },
         {
             "title" : "Intro to Physics",
             "school" : "Udacity",
-            "url" : "https://www.udacity.com/course/intro-to-physics--ph100"
+            "url" : "https://www.udacity.com/course/intro-to-physics--ph100",
+            "dates" : "2013"
         }
     ],
     "display" : function() {
@@ -151,13 +166,17 @@ var education = {
             var formattedName = HTMLschoolName.replace("%data%", school.name);
             var formattedDegree = HTMLschoolDegree.replace("%data%", school.degree);
             var nameDegree = formattedName + formattedDegree;
-            var formattedMajor = HTMLschoolMajor.replace("%data%", school.major);
             var formattedDates = HTMLschoolDates.replace("%data%", school.dates);
             var formattedLocation = HTMLschoolLocation.replace("%data%", school.location);
             $(".education-entry:last").append(nameDegree);
             $(".education-entry:last").append(formattedDates);
             $(".education-entry:last").append(formattedLocation);
-            $(".education-entry:last").append(formattedMajor);
+            school.majors.forEach(function(major) {
+                var formattedMajor = HTMLschoolMajor.replace("%data%", major);
+                $(".education-entry:last").append(formattedMajor);
+            });
+            var formattedURL = HTMLonlineURL.replace("%data%", school.url);
+            $(".education-entry:last").append(formattedURL);
         });
         if (this.onlineCourses.length > 0) {
             $("#education").append(HTMLonlineClasses);
@@ -165,8 +184,10 @@ var education = {
                 $("#education").append(HTMLschoolStart);
                 var formattedTitle = HTMLonlineTitle.replace("%data%", course.title);
                 var formattedSchool = HTMLonlineSchool.replace("%data%", course.school);
+                var formattedDates = HTMLonlineDates.replace("%data%", course.dates);
                 var formattedURL = HTMLonlineURL.replace("%data%", course.url);
                 $(".education-entry:last").append(formattedTitle + formattedSchool);
+                $(".education-entry:last").append(formattedDates);
                 $(".education-entry:last").append(formattedURL);
             });
         }
@@ -177,16 +198,24 @@ var projects = {
     "projects" : [
         {
             "title" : "FileUpload",
-            "description" : "JavaScript plugin for uploading files to a server."
+            "description" : "JavaScript plugin for uploading files to a server.",
+            "dates" : "2016 - current",
+            "images" : [ "images/me.jpg", "images/me.jpg" ]
         }
     ],
     "display" : function() {
         this.projects.forEach(function(project) {
             $("#projects").append(HTMLprojectStart);
             var formattedTitle = HTMLprojectTitle.replace("%data%", project.title);
+            var formattedDates = HTMLprojectDates.replace("%data%", project.dates);
             var formattedDescription = HTMLprojectDescription.replace("%data%", project.description);
             $(".project-entry:last").append(formattedTitle);
+            $(".project-entry:last").append(formattedDates);
             $(".project-entry:last").append(formattedDescription);
+            project.images.forEach(function(image) {
+                var formattedImage = HTMLprojectImage.replace("%data%", image);
+                $(".project-entry:last").append(formattedImage);
+            });
         });
     }
 };
